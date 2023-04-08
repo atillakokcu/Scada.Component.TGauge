75a8c0b4c746bbcfe26a7abd3ca8e027a7869804
atillakokcu <kokcu.ati@gmail.com>
2023-04-08 19:21:47 +0300

    Queue ekleme

--- Scada.Data.DataList/TScadaList.cs
+++ Scada.Data.DataList/TScadaList.cs
@@ -10,10 +10,31 @@ namespace Scada.Data.DataList
     public class TScadaList
     {
         public static List<TData> DataList = new List<TData>();
+        public static Queue<List<TData>> Datas = new Queue<List<TData>>();
 
         public void AddData(TData Data)
         {
             DataList.Add(Data);
         }
+
+        public static void AddHeatData(TData Data)
+        {
+            Datas.Enqueue(Data);
+        }
+
+
+        public static TData GetHeatDatas()
+        {
+            return Datas.Dequeue();
+
+        }
+
+        public void dd()
+        {
+            Datas.
+
+        }
+
+
     }
 }
