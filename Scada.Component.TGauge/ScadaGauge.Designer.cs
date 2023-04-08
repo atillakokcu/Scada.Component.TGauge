148657b16db440a905867336862844fe41a951d3
atillakokcu <kokcu.ati@gmail.com>
2023-04-08 12:10:32 +0300

    desing oluşturulması

---
+++ Scada.Component.TGauge/ScadaGauge.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Scada.Component.TGauge
+{
+    partial class ScadaGauge
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.LblName = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LblName
+            // 
+            this.LblName.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.LblName.Dock = System.Windows.Forms.DockStyle.Top;
+            this.LblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblName.Location = new System.Drawing.Point(0, 0);
+            this.LblName.Name = "LblName";
+            this.LblName.Size = new System.Drawing.Size(100, 20);
+            this.LblName.TabIndex = 0;
+            this.LblName.Text = "label1";
+            this.LblName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.trackBar1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 20);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(100, 250);
+            this.panel1.TabIndex = 1;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.trackBar1.LargeChange = 10;
+            this.trackBar1.Location = new System.Drawing.Point(0, 0);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(100, 250);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.TickFrequency = 10;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar1.Value = 15;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ScadaGauge
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.LblName);
+            this.Name = "ScadaGauge";
+            this.Size = new System.Drawing.Size(100, 270);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblName;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
---
+++ Scada.Component.TGauge/ScadaGauge.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scada.Component.TGauge
+{
+    public partial class ScadaGauge : UserControl
+    {
+        private int FHeat;
+
+        public int Heat
+        {
+            get { return GetHeat(); }
+            set { SetHeat(value); }
+        }
+
+        private void SetHeat(int value)
+        {
+            FHeat = value;
+            trackBar1.Value = FHeat;
+
+            if (FHeat >= FmaxHeat)
+            {
+                if (FAlarmAktive)
+                {
+                    timer1.Enabled= true;
+                }
+            }
+        }
+
+        private int GetHeat()
+        {
+            return FHeat;
+        }
+
+
+        private string FDeviceName;
+
+        public string DeviceName
+        {
+            get { return GetDeviceName(); }
+            set { SetDeviceName(value); }
+        }
+
+        private void SetDeviceName(string value)
+        {
+            FDeviceName = value;
+            LblName.Text = value;
+        }
+
+        private string GetDeviceName()
+        {
+            return DeviceName;
+        }
+
+        private int FmaxHeat;
+
+        public int MaxHeat
+        {
+            get { return GetmaxHeat(); }
+            set { SetmaxHeat(value); }
+        }
+
+        private void SetmaxHeat(int value)
+        {
+            FmaxHeat = value;
+        }
+
+        private int GetmaxHeat()
+        {
+            return FmaxHeat;
+        }
+
+
+        private bool FAlarmAktive;
+
+        public bool AlarmAktive
+        {
+            get { return GetAlarmAktive(); }
+            set { SetAlarmAktive(value); }
+        }
+
+        private void SetAlarmAktive(bool value)
+        {
+            FAlarmAktive = value;
+        }
+
+        private bool GetAlarmAktive()
+        {
+            return FAlarmAktive;
+        }
+
+        public ScadaGauge()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (true)
+            {
+
+            }
+        }
+    }
+}
